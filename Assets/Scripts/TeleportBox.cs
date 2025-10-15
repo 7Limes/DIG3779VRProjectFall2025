7ee72cbfb7639897494e3f8bc155e13b3ffb64c4
user <user@FIH-COM23a107.fsu.edu>
2025-10-15 16:29:23 -0400

    Add more objects to room

--- Assets/Scripts/TeleportBox.cs
+++ Assets/Scripts/TeleportBox.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class TeleportBox : MonoBehaviour
 {
     public Transform teleportPoint;
+    public bool relative = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
-            other.transform.root.position = teleportPoint.position;
+        {   
+            if (relative)
+            {
+                Vector3 relativeVector = other.transform.position - transform.position;
+                Vector3 newPosition = teleportPoint.position + relativeVector;
+                newPosition.y = teleportPoint.position.y;
+                other.transform.position = newPosition;
+            }
+            else
+            {
+                other.transform.position = teleportPoint.position;
+            }
         }
     }
 }
